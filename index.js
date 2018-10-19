ed0f7d19769605a2cbea19f6722d47d7e193b9d6
Princend <waxdji3g4y3c04@gmail.com>
2018-10-19 16:41:53 +0800

    getWeather

--- index.js
+++ index.js
@@ -12,26 +12,26 @@ var bot = linebot({
 var url = 'http://www.cwb.gov.tw/V7/forecast/taiwan/Taichung_City.htm';
 var timer;
 var pm = [];
-_getJSON();
-var totalstr=''
+// _getJSON();
+var totalstr = ''
 _bot();
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server = app.listen(process.env.PORT || 8080, function() {
+var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
 
 function _bot() {
-  bot.on('message', function(event) {
+  bot.on('message', function (event) {
     if (event.message.type == 'text') {
       var msg = event.message.text;
       var replyMsg = '';
       if (msg.indexOf('PM2.5') != -1) {
-        pm.forEach(function(e, i) {
+        pm.forEach(function (e, i) {
           if (msg.indexOf(e[0]) != -1) {
             replyMsg = e[0] + '的 PM2.5 數值為 ' + e[1];
           }
@@ -41,50 +41,87 @@ function _bot() {
         }
       }
       if (replyMsg == '') {
-        replyMsg = '不知道「'+msg+'」是什麼意思 :p';
+        replyMsg = '不知道「' + msg + '」是什麼意思 :p';
       }
-      if (msg.indexOf('天氣') != -1) { 
-        replyMsg = 'testin';
-        replyMsg =_getJSON();
+      if (msg.indexOf('天氣') != -1) {
+
+        this.getWeather();
+        // replyMsg = totalstr;
       }
-      event.reply(replyMsg).then(function(data) {
-        console.log(replyMsg);
-      }).catch(function(error) {
+      event.reply(replyMsg).then(function (data) {
+        if (msg.indexOf('天氣') != -1) {
+          setTimeout(() => {
+            replyMsg = totalstr;
+            console.log(replyMsg);
+          }, 1000);
+        }
+        else {
+          console.log(replyMsg);
+        }
+      }).catch(function (error) {
         console.log('error');
       });
     }
   });
-
 }
 
-function _getJSON() {
-  request('http://www.cwb.gov.tw/V7/forecast/taiwan/Taichung_City.htm', function (err, res, body) {    
-      var $ = cheerio.load(body);
-      var weather = []
-      totalstr = '';
-      $('.FcstBoxTable01 tbody tr').each(function (i, elem) {
-        weather.push($(this).text().split('\n'));
+
+getWeather()
+function getWeather() {
+  request('http://www.cwb.gov.tw/V7/forecast/taiwan/Taichung_City.htm', function (err, res, body) {
+    var $ = cheerio.load(body);
+    var weather = []
+    totalstr = '';
+    $('.FcstBoxTable01 tbody tr').each(function (i, elem) {
+      weather.push($(this).text().split('\n'));
+    });
+    var output = [];
+    for (var i = 0; i < 3; i++) {
+      output.push({
+        time: weather[i][1].substring(2).split(' ')[0],
+        temp: weather[i][2].substring(2),
+        rain: weather[i][6].substring(2)
       });
-      var output = [];
-      for (var i = 0; i < 3; i++) {
-        output.push({
-          time: weather[i][1].substring(2).split(' ')[0],
-          temp: weather[i][2].substring(2),
-          rain: weather[i][6].substring(2)
-        });
-      }
-  
-      for (var i = 0; i < output.length; i++) {
-        var time = output[i].time;
-        var temp = output[i].temp;
-        var rain = output[i].rain;
-        var str = time + '，溫度大約' + temp + '度，降雨機率 ' + rain + ';';
-        totalstr += str;
-      }
-      return totalstr;
-    })
-  // timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
+    }
+    for (var i = 0; i < output.length; i++) {
+      var time = output[i].time;
+      var temp = output[i].temp;
+      var rain = output[i].rain;
+      var str = time + '，溫度大約' + temp + '度，降雨機率 ' + rain + ';';
+      totalstr += str;
+    }
+  })
 }
+
+
+
+/* function _getJSON() {
+  clearTimeout(timer);
+  request('http://www.cwb.gov.tw/V7/forecast/taiwan/Taichung_City.htm', function (err, res, body) {
+    var $ = cheerio.load(body);
+    var weather = []
+    totalstr = '';
+    $('.FcstBoxTable01 tbody tr').each(function (i, elem) {
+      weather.push($(this).text().split('\n'));
+    });
+    var output = [];
+    for (var i = 0; i < 3; i++) {
+      output.push({
+        time: weather[i][1].substring(2).split(' ')[0],
+        temp: weather[i][2].substring(2),
+        rain: weather[i][6].substring(2)
+      });
+    }
+    for (var i = 0; i < output.length; i++) {
+      var time = output[i].time;
+      var temp = output[i].temp;
+      var rain = output[i].rain;
+      var str = time + '，溫度大約' + temp + '度，降雨機率 ' + rain + ';';
+      totalstr += str;
+    }
+  })
+  timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
+} */
 
 /* function _getJSON() {
 
