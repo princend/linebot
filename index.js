48b50cacddd46a77c5498b56b0f2ba616f751607
Princend <waxdji3g4y3c04@gmail.com>
2018-10-23 17:30:02 +0800

    早八發照片

--- index.js
+++ index.js
@@ -139,7 +139,9 @@ getFGOimg();
 
 function getFGOimg() {
   request(options, function (err, res, body) {
-    getImgs(body)
+    getImgs(body);
+    sendOptions.json.messages[0].originalContentUrl = photo;
+    sendOptions.json.messages[0].previewImageUrl = photo;
   })
 }
 
@@ -214,30 +216,34 @@ function getRandom(x) {
 
 
 var sendOptions = {
+  headers: { 'content-Type': 'application/json', 'Authorization': 'Bearer {NAq8fSKh+eKmAGR8iUqPmkLXAKI+mgjzi8pyCNyEktcxOfjfqCvlUQLzyLddcYsu3ql4eALNUt+ehby368gn1fjVGxuCTK1jRRIitof7Fc61rjepO662fZsvZpyZStfiU8RwSPIpEInUcvPEOFuZPwdB04t89/1O/w1cDnyilFU=}' },
   url: 'https://api.line.me/v2/bot/message/push',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer {NAq8fSKh+eKmAGR8iUqPmkLXAKI+mgjzi8pyCNyEktcxOfjfqCvlUQLzyLddcYsu3ql4eALNUt+ehby368gn1fjVGxuCTK1jRRIitof7Fc61rjepO662fZsvZpyZStfiU8RwSPIpEInUcvPEOFuZPwdB04t89/1O/w1cDnyilFU=}'
-  },
-  body:{
-      "to": "U88cd28005c1e4c187bd8bb50a4532bde",
-      "messages":[
-          {
-              "type":"text",
-              "text":"Hello, world1"
-          },
-          {
-              "type":"text",
-              "text":"Hello, world2"
-          }
-      ]
+  json: {
+    "to": "U88cd28005c1e4c187bd8bb50a4532bde", "messages": [{
+      "type": "image",
+      "originalContentUrl": "https://free.com.tw/blog/wp-content/uploads/2014/08/Placekitten480-g.jpg",
+      "previewImageUrl": "https://free.com.tw/blog/wp-content/uploads/2014/08/Placekitten480-g.jpg"
+    }]
   }
 };
 
 
-autoSendMsg();
-function autoSendMsg() {
-  request(sendOptions, function (err, res, body) {
-  })
 
+function autoSendMsg() {
+  getFGOimg();
+  setTimeout(() => {
+    request.post(sendOptions, (err) => { console.log('res', err); })
+  }, 1000)
+}
+
+timer()
+function timer() {
+  setInterval(() => {
+    let date = new Date().toLocaleTimeString();
+    if (date.substring(0, 7) === "08:00:0") {
+      if (Number(date.substring(7, 8) <= 4)) {
+        autoSendMsg();
+      }
+    }
+  }, 5000)
 }
